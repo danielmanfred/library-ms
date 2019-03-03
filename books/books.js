709d387544015ff7a77a570da6456f935f867756
danielmanfred <danielmanfred@gmail.com>
2019-03-03 00:17:52 -0300

    Creating routes

--- books/books.js
+++ books/books.js
@@ -5,12 +5,18 @@ const app = express()
 // Load mongoose
 const mongoose = require('mongoose')
 // Connect
-mongoose.connect('mongodb://manfred:0110seg@ds237700.mlab.com:37700/booksservice', () => {
-    console.log('Database is connected!')
-})
+mongoose.connect('mongodb://manfred:0110seg@ds237700.mlab.com:37700/booksservice', 
+    { useNewUrlParser: true }, 
+    () => console.log('Database is connected!')
+)
 
 app.get('/', (req, res) => {
     res.send('This is the books service')
+})
+
+// Create func
+app.post('/book', (req, res) => {
+    res.send('00:16')
 })
 
 app.listen(4545, () => {
